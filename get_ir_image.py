0a5b44ae5f5a047bc560cade08289e4c332bf773
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2023-09-21 14:07:33 +0900

    get image from ir camera

--- get_ir_image.py
+++ get_ir_image.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+import cv2
+from cv2 import add
+from flirpy.camera.boson import Boson
+import numpy as np
+import time
+import os
+import datetime
+
+with Boson() as camera:
+    tmp = camera.grab().astype(np.float32)
+    # Rescale to 8 bit
+    img = 255 * (tmp - tmp.min()) / (tmp.max() - tmp.min())
+    """ Apply colourmap - try COLORMAP_JET if INFERNO doesn't work. 
+        You can also try PLASMA or MAGMA
+        img_col = cv2.applyColorMap(img.astype(np.uint8), cv2.COLORMAP_INFERNO)
+    """
+    img = cv2.applyColorMap(img.astype(np.uint8), cv2.COLORMAP_INFERNO)
+    img = img.astype(np.uint8)
+
+    key = cv2.waitKey(1) & 0xFF
+    dt_now = datetime.datetime.now()
+    # y, m, d, h = dt_now.year, dt_now.month, dt_now.day, dt_now.hour
+    min, s = dt_now.minute, dt_now.second
+    name = "{}".format(dt_now.strftime("%Y%m%d_%H%M%S"))
+    name_tiff = name + ".tiff"
+
+    camera.do_ffc()
+    # np.save(name, tmp)
+    time.sleep(1)
+    # 画像を保存
+    # cv2.imwrite("capture/" + name_tiff, img)
+    cv2.imwrite(name_tiff, img)
+    print("Saved {} & {}".format(name, name_tiff))
